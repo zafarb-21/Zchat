4e13017fc0852324c18a21ec781382e96c1aa498
zafarb-21 <766cwqdyq2@privaterelay.appleid.com>
2026-03-12 22:29:25 -0400

    Stop tracking node_modules and build output

--- server/dist/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-// server/src/db.ts
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.prisma = prisma;
-exports.default = prisma;
